717ed292902d963c1c845fea87c5a6792ba8954b
Liuyuting5 <944379719@qq.com>
2015-07-07 20:07:57 +0800

    gggg

---
+++ ting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ting
+//
+//  Created by qianfeng on 15/7/7.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ting/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ting
+//
+//  Created by qianfeng on 15/7/7.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [[UIButton alloc] init];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
